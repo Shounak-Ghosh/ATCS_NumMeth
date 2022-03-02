362d898765ff10bebd55674da5146b262811ca63
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-03-01 17:03:16 -0800

    reorganize file structure

---
+++ 3-Finding Zeros/finding_zeros.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+color_list = ['r', 'b', 'g', 'y', 'm', 'k', 'c', 'b']
+
+
+def function_list(x):
+    return [x ** 2, 1 + x ** 2, math.exp(-x ** 2), x * math.exp(-x ** 2),
+            x * math.exp(x ** 2), x * math.exp(-x), x * math.exp(x),
+            math.sqrt(abs(x))]
+
+
+def generate_function_point_list():
+    time_points = np.linspace(-10, 10, 100)
+    point_list = []
+    for t in time_points:
+        vals = function_list(t)
+        vals.insert(0, t)
+        point_list.append(vals)
+    return point_list
+
+
+def plot_functions(point_list):
+    num_functions = len(point_list[0]) - 1
+    time_list = []
+    val_list = []
+    for i in range(num_functions):
+        val_list.append(list())
+
+    for point in point_list:
+        time_list.append(point[0])
+        for i in range(1, num_functions+1):
+            val_list[i-1].append(point[i])
+
+    # print(time_list)
+    # print(val_list[7])
+    # print(len(time_list), len(val_list[7]))
+    # i = 7
+    # plt.scatter(time_list, val_list[i], color=color_list[i])
+    # plt.plot(time_list, val_list[i], color=color_list[i])
+    # plt.draw()
+
+
+
+    # for point in point_list:
+    #     for i in range(1, num_functions):
+    #         plt.plot(point[0], point[i], color=color_list[i])
+    #         plt.scatter(point[0], point[i], color=color_list[i])
+    #         plt.draw()
+
+
+def main():
+    # Set the plot boundaries
+    plt.figure("Functions", figsize=(8, 8))
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
+    plt.xlabel("time")
+    plt.ylabel("values")
+    plot_functions(generate_function_point_list())
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
