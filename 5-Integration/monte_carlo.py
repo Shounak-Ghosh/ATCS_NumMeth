401c7c538672768849161a1e9c27f419843e430e
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-04-13 13:56:24 -0700

    added monte carlo plot and 3d histogram

--- 5-Integration/monte_carlo.py
+++ 5-Integration/monte_carlo.py
@@ -2,7 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
-def main():
+
+def monte_carlo(dimensions, num_iterations):
+    N = np.random.uniform(-1, 1, (dimensions, num_iterations))
+    num_in_circle = 0
+
+    for i in range(num_iterations):
+        if np.linalg.norm(N[:, i]) <= 1:
+            num_in_circle += 1
+
+    print("Ratio:", num_in_circle / num_iterations)
+
+    # random number histogram
+    plt.figure("Random Number Histogram", figsize=(5, 5))
+    plt.hist(N[0], bins=10)
+    plt.show()
+
+
+def plot_monte_carlo():
     plt.figure("Monte Carlo Plot", figsize=(5, 5))
 
     # plot circle
@@ -10,27 +27,46 @@ def main():
     x = np.cos(t)
     y = np.sin(t)
     plt.plot(x, y, 'k-')
-    
 
-    #generate random points
-    x = np.random.uniform(-1, 1, 1000)
-    y = np.random.uniform(-1, 1, 1000)
+    # generate random points
+    num_iter = int(1e3)
+    N = np.random.uniform(-1, 1, (2, num_iter))
     num_in_circle = 0
 
-    for i in range(len(x)):
-        if x[i]**2 + y[i]**2 <= 1:
-            plt.scatter(x[i], y[i], color='red')
+    for i in range(num_iter):
+        curr = N[:, i]
+        if  np.linalg.norm(curr) <= 1:
+            plt.scatter(curr[0], curr[1], color='red')
             num_in_circle += 1
         else:
-            plt.scatter(x[i], y[i], color='blue')
+            plt.scatter(curr[0], curr[1], color='blue')
         plt.draw()
         plt.pause(1e-7)
 
-    print("Pi (approximation): ", 4 * num_in_circle / len(x))
+    print("Ratio:", num_in_circle / num_iter)
+    print("Pi (Approximation):", 4 * num_in_circle / int(1e6))
+
+    # random number histogram
+    fig = plt.figure("Random Number Histogram", figsize=(5, 5))
+    ax = fig.add_subplot(projection='3d')
+    hist, xedges, yedges = np.histogram2d(N[0], N[1], bins=6, range=[[-1, 1], [-1, 1]])
+    # Construct arrays for the anchor positions 
+    xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
+    xpos = xpos.ravel()
+    ypos = ypos.ravel()
+    zpos = 0
+
+    # Construct arrays with the dimensions for the bars
+    dx = dy = 0.5 * np.ones_like(zpos)
+    dz = hist.ravel()
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average')
     
     plt.show()
 
-    
+def main():
+    # monte_carlo(3, int(1e6))
+    plot_monte_carlo()
+
 
 # main
 if __name__ == "__main__":
